fd3c034c5d14dd403445449f8d227342d83f928e
Sergey <you@example.com>
2015-11-19 00:33:41 +0200

    to production

--- app/main.js
+++ app/main.js
@@ -15,6 +15,7 @@ angular.module('newsFacts', [
             time:'12:32 PM',
             date:'11 jan 2018',
             countryName:'Japan',
+            countryFlag:'images/flags/Japan.png',
             text:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum'
         }
     });
